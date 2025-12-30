1afc83a3b81921109e63fc6d9b8475bc5c9b29b1
Eryck Assis <eng.assis.dev@gmail.com>
2025-12-30 16:50:18 -0300

    feat: implement initial pagination component

---
+++ components/pagination.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+export function Pagination({
+  currentPage,
+  totalPages,
+  onPageChange,
+}: PaginationProps) {
+  const getPageNumbers = () => {
+    const pages: (number | string)[] = [];
+    const maxVisiblePages = 7;
+  };
+}
